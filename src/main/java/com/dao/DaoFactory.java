8409dce5194b98b2b9016400a3d109d6d9fc4a20
godallgi <gillian.godallier@reseau.eseo.fr>
2022-11-30 16:22:35 +0100

    ajout des proprietes dans le fichier properties

--- src/main/java/com/dao/DaoFactory.java
+++ src/main/java/com/dao/DaoFactory.java
@@ -1,17 +1,33 @@
 package com.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DaoFactory {
 	private String url;
     private String username;
     private String password;
+    private static final Logger logger 
+	= LoggerFactory.getLogger(DaoFactory.class);
+    private Properties props = new Properties();
     
-	DaoFactory(String url, String username, String password) {
-        this.url = url;
-        this.username = username;
-        this.password = password;
+	DaoFactory() {
+		
+		try {
+			props.load(DaoFactory.class.getResourceAsStream("/Application.properties"));
+		} catch (IOException e) {
+			logger.info("Cannot read properties file");
+		}
+		
+        this.url = props.getProperty("databaseURL");
+        this.username = props.getProperty("databaseUsername");
+        this.password = props.getProperty("databasePassword");
     }
     
     public static DaoFactory getInstance() {
@@ -20,14 +36,13 @@ public class DaoFactory {
         } catch (ClassNotFoundException e) {
 
         }
-
-        DaoFactory instance = new DaoFactory(
-                "jdbc:mysql://localhost:3306/villes?useUnicode=true&characterEncoding=utf8", "root", "");
+        DaoFactory instance = new DaoFactory();
         return instance;
     }
 
     public java.sql.Connection getConnection() throws SQLException {
     	java.sql.Connection connexion= DriverManager.getConnection(url, username, password);
+    	System.out.println(url);
     	connexion.setAutoCommit(false);
         return connexion;
     }
